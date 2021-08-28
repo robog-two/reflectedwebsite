f1d0d3050850a7f076258bc06958b9c0c3cac5ac
Sam Knight <robogamingbysam@icloud.com>
2021-08-28 01:17:35 -0500

    Better multiplatform support, still needs video

--- javascript/platforms.js
+++ javascript/platforms.js
@@ -3,5 +3,5 @@ if (navigator.userAgent.match(/\(.*Windows.*\)/g)) {
 } else if (navigator.userAgent.match(/\(.*Ubuntu.*\)/g)) {
   document.getElementById("downloadlink").href = "https://rnwk.xyz/p9abg"
 } else {
-  document.getElementById("downloadcontainer").innerHTML = "<p>Minecraft, Unified</p><span>IP: <span class=\"copyable\">reflected.network</span></span>"
+  document.getElementById("downloadcontainer").innerHTML = "<p>We weren't kidding!<br />Bedrock or Java,<br />From Anywhere</p><span class=\"ipspan\">IP: <div class=\"copyable\">reflected.network</div></span><br /><div class=\"bedrocklinks\">Instructions for <a href=\"https://www.youtube.com/watch?v=g8mHvasVHMs\">Xbox</a> or <a href=\"https://www.youtube.com/watch?v=zalT_oR1nPM\">Switch</a></div>"
 }
