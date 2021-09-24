c3eeb81305d9ae476066be721fe21c0522e1624a
Sam Knight <robogamingbysam@icloud.com>
2021-09-24 17:33:29 -0500

    optimize website loading pass 2

--- javascript/carousel.js
+++ javascript/carousel.js
@@ -3,7 +3,7 @@ let carouselPos = 1
 async function rotateCarousel() {
   let carousel = document.getElementById('carousel')
 
-  carousel.style.backgroundImage = `url('${`images/carousel/slide${carouselPos}.png`}')`
+  carousel.style.backgroundImage = `url('${`images/carousel/slide${carouselPos}.avif`}')`
 
   carouselPos++
   if (carouselPos > 5) {
@@ -12,10 +12,6 @@ async function rotateCarousel() {
 }
 
 async function loaded() {
-  for (el of document.getElementsByClassName("preload")) {
-    el.style.display = 'none'
-  }
-
   window.setInterval(rotateCarousel, 4000)
   rotateCarousel()
 }
